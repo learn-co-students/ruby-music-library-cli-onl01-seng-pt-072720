ca6a8d9a4949cf73d2011e34e4869afbcece89c2
Shalom Kebede <shalomseleshi@gmail.com>
2020-09-03 04:56:36 +0000

    Done.

--- lib/models/song.rb
+++ lib/models/song.rb
@@ -1,12 +1,13 @@
 class Song 
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, :genre
   
   
   @@all = []
   
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist = artist if artist!=nil
+    @genre = genre
   end
   
   def self.all 
@@ -26,18 +27,16 @@ class Song
     song.save
     song
   end
-  
+ 
   def artist=(artist)
     @artist = artist
     artist.add_song(self)
     artist
-    
-    # name.add_song(self)
-    # if artist != nil 
-    #   # self.artist = artist
-    #   artist.name.add_song(self)
-    # end
+  end
   
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self
   end
   
   
