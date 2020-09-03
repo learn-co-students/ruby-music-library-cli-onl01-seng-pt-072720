3a2e6f90a0a83157c31eef947a7f975dcb4951a6
Shalom Kebede <shalomseleshi@gmail.com>
2020-09-03 22:03:52 +0000

    Done.

--- lib/models/MusicLibraryController.rb
+++ lib/models/MusicLibraryController.rb
@@ -13,7 +13,7 @@ class MusicLibraryController
     
     input = gets.strip
     
-    if input != exit
+    # if input != exit
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of the genres in your library, enter 'list genres'."
@@ -22,7 +22,7 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-    end
+    # end
     
   end
 end
