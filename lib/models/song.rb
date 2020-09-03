03590a37f2bd4ad55d829caec6bc69d388a08e69
Shalom Kebede <shalomseleshi@gmail.com>
2020-09-03 21:27:48 +0000

    Done.

--- lib/models/song.rb
+++ lib/models/song.rb
@@ -60,8 +60,8 @@ class Song
     
   end
   
-  def self.create_from_filename 
-    self.new_from_filename.save
+  def self.create_from_filename(filename) 
+    self.new_from_filename(filename).save
   end
   
 end
