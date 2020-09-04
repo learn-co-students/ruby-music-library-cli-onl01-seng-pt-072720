5e5ed5a20ef1b63918b0e85ca5b9332e06bd5276
Shalom Kebede <shalomseleshi@gmail.com>
2020-09-04 01:05:55 +0000

    Done.

--- lib/models/MusicLibraryController.rb
+++ lib/models/MusicLibraryController.rb
@@ -35,9 +35,11 @@ class MusicLibraryController
       end 
       
       def list_songs_by_artist
+        
       end 
       
       def list_songs_by_genre
+        puts "Please enter the name of an artist:"
       end 
       
       def play_song 
