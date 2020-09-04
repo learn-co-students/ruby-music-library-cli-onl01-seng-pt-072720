2273083918e8fda15a0384ec6bcbdd07a657160b
cinna-codes <somethingfellipickeditup@gmail.com>
2020-09-03 21:18:38 -0400

    Done.

--- lib/models/music_library_controller.rb
+++ lib/models/music_library_controller.rb
@@ -7,18 +7,27 @@ class MusicLibraryController
     end
 
     def call
-        puts "Welcome to your music library!"
-        puts "To list all of your songs, enter 'list songs'."
-        puts "To list all of the artists in your library, enter 'list artists'."
-        puts "To list all of the genres in your library, enter 'list genres'."
-        puts "To list all of the songs by a particular artist, enter 'list artist'."
-        puts "To list all of the songs of a particular genre, enter 'list genre'."
-        puts "To play a song, enter 'play song'."
-        puts "To quit, type 'exit'."
-        puts "What would you like to do?"
-        gets.chomp
+        input = ""
+        until input == 'exit'
+            puts "Welcome to your music library!"
+            puts "To list all of your songs, enter 'list songs'."
+            puts "To list all of the artists in your library, enter 'list artists'."
+            puts "To list all of the genres in your library, enter 'list genres'."
+            puts "To list all of the songs by a particular artist, enter 'list artist'."
+            puts "To list all of the songs of a particular genre, enter 'list genre'."
+            puts "To play a song, enter 'play song'."
+            puts "To quit, type 'exit'."
+            puts "What would you like to do?"
+            input = gets.strip
 
-        gets until gets == 'exit'
+            list_songs if input == 'list songs'
+            list_artists if input == 'list artists'
+            list_genres if input == 'list genres'
+            list_songs_by_artist if input == 'list artist'
+            list_songs_by_genre if input == 'list genre'
+            play_song if input == 'play song'
+
+        end
     end
 
     def list_songs
@@ -51,15 +60,12 @@ class MusicLibraryController
         puts "Which song number would you like to play?"
         song_number = gets.strip
         index_number = song_number.to_i - 1 
-        if index_number.between?(0, Song.all.length) 
+        if index_number.between?(0, Song.all.length-1)
             song_to_play = Song.all.sort_by { |s| s.name }[index_number]
             puts "Playing #{song_to_play.name} by #{song_to_play.artist.name}"
         end
-        # song_to_play = list_songs[index_number].split(" ")
-        # puts "Playing #{song_to_play[3]} by #{song_to_play[1]}"
-        # # ["num.[0]", "song.artist.name[1]", "-[2]", "song.name[3]", "-[4]", "song.genre.name[5]"]
-        # # puts "Playing #{song_name} by #{artist_name}"
-        # binding.pry
     end
-    # binding.pry
+
+
+
 end
