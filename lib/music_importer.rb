4eaa8933f2d2abe1e7a280c945925c05f2c19da7
sergiocardenasg <sergiocardenas@utexas.edu>
2020-09-04 13:48:55 -0500

    Done.

---
+++ lib/music_importer.rb
@@ -0,0 +1,16 @@
+class MusicImporter
+    attr_accessor :path
+
+    def initialize(path)
+        @path = path
+    end
+
+    def files
+        @files = Dir["#{path}/*"].collect{|f|f.gsub("#{path}/", "") }
+    end
+
+    def import
+        files.each{|file|Song.create_from_filename(file)}
+    end
+
+end
