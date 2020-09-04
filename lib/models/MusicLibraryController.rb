6ec6da3e442fce866f17cf31a23a928feecf5df5
Shalom Kebede <shalomseleshi@gmail.com>
2020-09-04 01:15:23 +0000

    Done.

--- lib/models/MusicLibraryController.rb
+++ lib/models/MusicLibraryController.rb
@@ -35,11 +35,19 @@ class MusicLibraryController
       end 
       
       def list_songs_by_artist
-        
+        puts "Please enter the name of an artist:"
+        input = gets.strip
+        if artist = Artist.find_by_name(input)
+          artist.songs.sort{ |a, b| a.name <=> b.name }.each_with_index{|s, i| puts "#{i+1}. #{s.name} - #{s.genre.name}"}
+        end
       end 
       
       def list_songs_by_genre
-        puts "Please enter the name of an artist:"
+        puts "Please enter the name of a genre:"
+        input = gets.strip
+        if genre = Genre.find_by_name(input)
+          genre.songs.sort{ |a, b| a.name <=> b.name }.each_with_index{|s, i| puts "#{i+1}. #{s.name} - #{s.genre.name}"}
+        end
       end 
       
       def play_song 
