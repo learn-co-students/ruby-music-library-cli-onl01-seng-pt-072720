588615a07e523fc98083bd12451b780d1340654d
Cody Merritt <cody.merritt@icloud.com>
2020-09-04 19:33:37 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+end
