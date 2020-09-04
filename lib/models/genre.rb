ffec9786006bf975fd3cd5956584d233aa164292
jmw0426 <jmw0426@hotmail.com>
2020-09-04 17:03:45 -0500

    Done.

---
+++ lib/models/genre.rb
@@ -0,0 +1,42 @@
+class Genre
+    extend Concerns::Findable
+    extend Persistable::ClassMethods
+    extend Nameable::ClassMethods
+    include Persistable::InstanceMethods
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def self.all
+        @@all
+    end
+
+   def self.destroy_all
+       @@all.clear
+   end
+
+    def self.create(name)
+        new_genre = self.new(name)
+        new_genre.save
+        new_genre
+    end
+
+    def add_song(song)
+        if song.artist == nil
+            song.artist = self
+        end
+        if !@songs.include?(song)
+            @songs << song
+        end
+    end
+
+    def artists
+        songs.collect {|song|song.artist}.uniq
+    end
+end
---
+++ lib/music/musicimporter.rb
@@ -0,0 +1,17 @@
+class MusicImporter
+    attr_accessor :path
+    
+    def initialize(path)
+        @path = path
+    end
+
+    def files
+        var = Dir.entries(self.path)
+        var.shift(2)
+        var
+    end
+
+    def import
+        self.files.each { |file| Song.create_from_filename(file) }
+    end
+end
