ab529adf33a4b1bc718fc8e2f7f09c0596c7b7c2
danainjax <danainjax@gmail.com>
2020-09-09 11:08:48 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,61 @@
+class Artist
+@@all = []
+extend Concerns::Findable
+    attr_accessor :name, :song
+    
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.create(name)
+      new_artist = new(name)
+      new_artist.save
+      new_artist  
+    end
+
+    def songs
+        @songs
+    end
+
+    def add_song(song)
+        if song.artist == nil
+            song.artist = self
+          else
+            nil
+          end
+          if @songs.include?(song)
+            nil
+          else
+            @songs << song
+          end
+          song
+    end
+
+    def self.destroy_all
+        @@all = []
+    end
+
+    def genres
+        @new_array = []
+        @songs.each do |song|
+      if @new_array.include?(song.genre)
+        nil
+      else
+        @new_array << song.genre
+      end
+    end
+    @new_array
+  end
+    
+
+end
