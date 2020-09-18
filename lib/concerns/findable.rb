2dc85da62c107ac41dc8506c6893702264fb67c6
AndrianaCodes <andriana.amg@gmail.com>
2020-09-18 13:55:11 -0400

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,19 @@
+module Concerns::Findable
+    def find_by_name(name)
+      self.all.detect{|s| s.name == name}
+    end
+  
+    def find_or_create_by_name(name)
+      self.find_by_name(name) || self.create(name)
+    end
+  end
+  
+  # def self.find_by_name(name)
+  #       #@@all.detect { |song| song.name == name } 
+  #       self.all.find {|song| song.name == name}
+  #     end 
+  
+  #     def self.find_or_create_by_name(name)
+  #       !self.find_by_name(name) ? self.create(name) : self.find_by_name(name)
+  #       # no .all method? 
+  #     end  
